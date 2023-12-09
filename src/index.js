120bea023ae8c0116efe2dd5742b9df0d8b45cec
1Kill2Steal <imliterallyhim@duck.com>
2023-12-09 19:21:31 +0200

    added a quote command

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 require('dotenv').config(); // CHECK README.md
-const { Client, IntentsBitField } = require('discord.js');
+const { Client, IntentsBitField, messageLink } = require('discord.js');
 
 const client = new Client({
   intents: [
@@ -14,14 +14,55 @@ client.on('ready', (c) => {
   console.log(`${c.user.tag} is online!`)
 })
 
+/// ! ! ! VARIABLES FOR COMMANDS ! ! !
+
+const pengooQuotes = [
+  "hu tao is fucking cute i cant",
+  "# yes",
+  "its pengover",
+  "ah-",
+  "LISSEN",
+  "sweet dreams"
+]
+  
+
+
+
+// Actual commands:
+
+// "Hu Tao" msg response;
+
 client.on('messageCreate', (message) => {
   if(message.author.bot) {
     return;
   }
 
   if (message.content === 'Hu Tao') {
-    message.reply('HU TAO BEST GIRL! <3')
+    message.reply('HU TAO BEST GIRL! <3');
   }
 })
+
+
+
+// SLASH COMMANDS
+
+client.on('interactionCreate', (interaction) => {
+  if(!interaction.isChatInputCommand()) return;
+
+  // console.log(interaction.commandName);
+
+  switch(interaction.commandName) {
+    case 'hey':
+      interaction.reply('hey!');
+      break;
+    case 'quote':
+      const randomQuoteIndex = Math.floor(Math.random() * pengooQuotes.length);
+      const randomQuote = pengooQuotes[randomQuoteIndex];
+      interaction.reply(randomQuote);
+    default:
+      break;
+  }
+});
+
 
 client.login(process.env.TOKEN);
