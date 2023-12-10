ff351dd052d7f9cb957ac175e5b2bbd437a379c4
1Kill2Steal <imliterallyhim@duck.com>
2023-12-10 22:08:30 +0200

    added !tieup !hug !pat

--- src/index.js
+++ src/index.js
@@ -16,7 +16,27 @@ client.on('ready', (c) => {
 
 /// ! ! ! VARIABLES FOR COMMANDS ! ! !
 
-
+const pengooQuotes = [
+  "hu tao is fucking cute i cant",
+  "# yes",
+  "https://media.discordapp.net/stickers/1181980659825262662.webp?size=160", // "its pengover",
+  "ah-",
+  "LISSEN",
+  "sweet dreams",
+  `Yo rizz me up? Umm, okay, I assume you meant to say "Tie me up" right? Uhm, well, I'm really not sure about that but-`,
+  "WAIT I LOVE HOW GETTING TIED UP IS ACTUALLY MY WHOLE PERSONALITY NOW LOL"
+]
+const pengooHugs = [
+  "https://cdn.discordapp.com/attachments/614790390020833280/1183462503364186112/hug.gif?ex=65886c58&is=6575f758&hm=275d61937ad2b529274870ad670319c4f978513f9442e19e74e698ca7fb88448&",
+  "https://cdn.discordapp.com/attachments/614790390020833280/1183462503011844096/anime-hug-anime-hugging.gif?ex=65886c58&is=6575f758&hm=a4138a57846d2ceeb75110e622353ce7506bf7f624d6a5f061e54dcefceb647b&",
+  "https://cdn.discordapp.com/attachments/614790390020833280/1183462502630174740/hug-surprise-chuunibyou.gif?ex=65886c58&is=6575f758&hm=311ad33b7ca80fe4a352a725b49ba29968ea17713d83b4292b2af0b64ee789f7&"
+]
+const pengooPats = [
+  "https://cdn.discordapp.com/attachments/614790390020833280/1183461602700316673/kanna-kamui-pat.gif?ex=65886b81&is=6575f681&hm=db97fec641c2d019b14696ef63ec2f66f01e56f47645c6200cefbf73b788b43b&",
+  "https://cdn.discordapp.com/attachments/614790390020833280/1183461632718950460/pat.gif?ex=65886b88&is=6575f688&hm=b7b9d42ed41507c586fee897ca362b075f5fb1ea976ed7ce259a69cb24c71f4e&",
+  "https://cdn.discordapp.com/attachments/614790390020833280/1183461661181497364/mai-sakurajima.gif?ex=65886b8f&is=6575f68f&hm=bd9a84bd007425cbf785bd28183c2ed32c2da97a254dc944f13e4e6b0b84bf63&",
+  "https://cdn.discordapp.com/attachments/614790390020833280/1183493730339139694/hu-tao-hug.gif?ex=6588896d&is=6576146d&hm=f90650df440c5b14a3f965b8448a91f393539739dfdfb98bcceb956b384dd029&"
+]
   
 
 
@@ -26,12 +46,13 @@ client.on('ready', (c) => {
 
 // MESSAGE COMMANDS (ADAPTATION FOR SLASH COMMANDS - NOT USING)
 
-/*
+
 client.on('messageCreate', (message) => {
   if(message.author.bot) {
     return;
   }
   
+  /*
   // plain command testing
 
   if (message.content === 'Hu Tao') {
@@ -61,35 +82,71 @@ client.on('messageCreate', (message) => {
       return;
     }
   }
+  */
+ 
+
+  // Declare userToInteract for the entire scope of messageCreate
+  const userToInteract = message.mentions.users.first();
+  const invoker = message.author;
+
+  // Command for tying up a user
+  if ( (message.content.toLowerCase() === '!tieup' && userToInteract) && message.member) {
+    if (invoker !== userToInteract) {
+      const tieupEmbed = new EmbedBuilder()
+        .setColor('#ff6d66')
+        .setImage('https://cdn.discordapp.com/attachments/614790390020833280/1183349571468918814/tied-up-aiura.gif?ex=6588032b&is=65758e2b&hm=b92c39af90ca21bbbc2965487a418f89e5ff9fef02f2ad5722cbfb0bf0cbb3c1&');
+
+      message.channel.send({
+        content: `*${invoker.toString()} ties up ${userToInteract.toString()}*`,
+        embeds: [tieupEmbed],
+      });
+    } else {
+      message.channel.send(`${userToInteract.toString()} Why do you wanna tie yourself up mate?\n||*please tie me up instead~~!*||`);
+    }
+    return;
+  }
+
+  // Command for hugging a user
+  if ( (message.content.toLowerCase() === '!hug' && userToInteract) && message.member) {
+    const randomHugIndex = Math.floor(Math.random() * pengooHugs.length);
+    const randomHugGIF = pengooHugs[randomHugIndex];
+
+    const hugEmbed = new EmbedBuilder()
+    .setColor('#ff6d66')
+    .setImage(`${randomHugGIF}`);
+
+    message.channel.send({
+      content: `*${invoker.toString()} hugs ${userToInteract.toString()}*`,
+      embeds: [hugEmbed],
+    });
+    return;
+  }
+
+  // Command for patting a user
+  if ( (message.content.toLowerCase() === '!pat' && userToInteract) && message.member) {
+    const randomPatIndex = Math.floor(Math.random() * pengooPats.length);
+    const randomPatGIF = pengooPats[randomPatIndex];
+
+    const patEmbed = new EmbedBuilder()
+    .setColor('#ff6d66')
+    .setImage(`${randomPatGIF}`);
+
+    message.channel.send({
+      content: `*${invoker.toString()} pats ${userToInteract.toString()}*`,
+      embeds: [patEmbed],
+    });
+    return;
+  }
   
 })
-*/
+
 
 // SLASH COMMANDS (USING)
 
 client.on('interactionCreate', async (interaction) => {
   if(!interaction.isChatInputCommand()) return;
 
-  const pengooQuotes = [
-    "hu tao is fucking cute i cant",
-    "# yes",
-    "https://media.discordapp.net/stickers/1181980659825262662.webp?size=160", // "its pengover",
-    "ah-",
-    "LISSEN",
-    "sweet dreams",
-    `Yo rizz me up? Umm, okay, I assume you meant to say "Tie me up" right? Uhm, well, I'm really not sure about that but-`,
-    "WAIT I LOVE HOW GETTING TIED UP IS ACTUALLY MY WHOLE PERSONALITY NOW LOL"
-  ]
-  const pengooHugs = [
-    "https://cdn.discordapp.com/attachments/614790390020833280/1183462503364186112/hug.gif?ex=65886c58&is=6575f758&hm=275d61937ad2b529274870ad670319c4f978513f9442e19e74e698ca7fb88448&",
-    "https://cdn.discordapp.com/attachments/614790390020833280/1183462503011844096/anime-hug-anime-hugging.gif?ex=65886c58&is=6575f758&hm=a4138a57846d2ceeb75110e622353ce7506bf7f624d6a5f061e54dcefceb647b&",
-    "https://cdn.discordapp.com/attachments/614790390020833280/1183462502630174740/hug-surprise-chuunibyou.gif?ex=65886c58&is=6575f758&hm=311ad33b7ca80fe4a352a725b49ba29968ea17713d83b4292b2af0b64ee789f7&"
-  ]
-  const pengooPats = [
-    "https://cdn.discordapp.com/attachments/614790390020833280/1183461602700316673/kanna-kamui-pat.gif?ex=65886b81&is=6575f681&hm=db97fec641c2d019b14696ef63ec2f66f01e56f47645c6200cefbf73b788b43b&",
-    "https://cdn.discordapp.com/attachments/614790390020833280/1183461632718950460/pat.gif?ex=65886b88&is=6575f688&hm=b7b9d42ed41507c586fee897ca362b075f5fb1ea976ed7ce259a69cb24c71f4e&",
-    "https://cdn.discordapp.com/attachments/614790390020833280/1183461661181497364/mai-sakurajima.gif?ex=65886b8f&is=6575f68f&hm=bd9a84bd007425cbf785bd28183c2ed32c2da97a254dc944f13e4e6b0b84bf63&"
-  ]
+  
   
   const userToInteract = interaction.options.getUser('user');
   const invoker = interaction.user;
@@ -97,7 +154,7 @@ client.on('interactionCreate', async (interaction) => {
   switch(interaction.commandName) {
     case 'quote':
       const index = interaction.options.getInteger('quote_number') - 1;
-      
+
       const randomIndex = ( index >= 0 && (index < pengooQuotes.length - 1) ) ? (index) : ( Math.floor(Math.random() * pengooQuotes.length) );
 
       const selectedQuote = pengooQuotes[randomIndex];
