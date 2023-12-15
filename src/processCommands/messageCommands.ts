678c1e353f17c24c79d415505e76c5ab9839da6f
1Kill2Steal <imliterallyhim@duck.com>
2023-12-15 21:27:43 +0200

    reimplemented message commands with switch

--- src/processCommands/messageCommands.ts
+++ src/processCommands/messageCommands.ts
@@ -6,6 +6,7 @@
 
 
 import { Client, Message, User } from "discord.js";
+import { COMMAND_PREFIX } from "../data/data";
 require('../functions')
 const messageCommands = require('../initializeCommands/messageCommands'); 
 const data = require('../data/data')
@@ -13,95 +14,89 @@ const data = require('../data/data')
 
 module.exports = async (client: Client, message: Message, invoker: User) => {
 
-  if(message.author.bot) {
+  if(!message.content.startsWith(COMMAND_PREFIX) || message.author.bot) {
     return;
   }
 
   // Entire scope of messageCreate so i can reuse it
   const userToInteract = message.mentions.users.first();
 
-  // Help command. Gives a small description about the commands and lists all of them.
-  if (message.content.toLowerCase() === `${data.COMMAND_PREFIX}${data.commandArray[0].name}`) {
-    await messageCommands.helpMessageCommand(message, data.commandListHeading, data.commandList);
-    return;
-  }
+
+  switch (message.content.toLowerCase()) {
+// Help command. Gives a small description about the commands and lists all of them.
+    case `${COMMAND_PREFIX}${data.commandArray[0].name}`:
+      messageCommands.helpMessageCommand(message, data.commandListHeading, data.commandList);
+    break;
 
 
-  // Command for printing out a random quote
-  if( (message.content.toLowerCase() === `${data.COMMAND_PREFIX}${data.commandArray[1].name}`) && message.member) {
-    await messageCommands.quoteMessageCommand(message, data.quoteArray);
-    return;
-  }
+    // Quote command. Used a different random index picker because i dont reuse this exact code archetype yet to make it a seperate function.
+    case `${COMMAND_PREFIX}${data.commandArray[1].name}`:
+      messageCommands.quoteMessageCommand(message, data.quoteArray);
+    break;
 
-  // Command for printing out the list of quotes
-  if ( (message.content.toLowerCase() === `${data.COMMAND_PREFIX}${data.commandArray[2].name}`) && message.member) {
-    await messageCommands.quotelistMessageCommand(message, data.quoteListHeading, data.quoteList);
-    return;
-  }
+    // Lists out all available quotes, "ephemeral" means only visible to the invoicer (https://discordjs.guide/slash-commands/response-methods#ephemeral-responses)
+    case `${COMMAND_PREFIX}${data.commandArray[2].name}`:
+      messageCommands.quotelistMessageCommand(message, data.quoteListHeading, data.quoteList);
+    break;
 
-  // Command for tying up a user
-  if ( (message.content.toLowerCase() === `${data.COMMAND_PREFIX}${data.commandArray[3].name}` && userToInteract) && message.member) {
-    await messageCommands.tieupMessageCommand(message, data.tieupArray, invoker, userToInteract);
-    return;
-  }
+    // Message command for tying up the user (analogous to !tieup)
+    case `${COMMAND_PREFIX}${data.commandArray[3].name}`:
+      messageCommands.tieupMessageCommand(message, data.tieupArray, invoker, userToInteract);
+    break;
 
-  // Command for hugging a user
-  if ( (message.content.toLowerCase() === `${data.COMMAND_PREFIX}${data.commandArray[4].name}` && userToInteract) && message.member) {
-    await messageCommands.hugMessageCommand(message, data.hugArray, invoker, userToInteract);
-    return;
-  }
+    // Message command for hugging the user (analogous to !hug)
+    case `${COMMAND_PREFIX}${data.commandArray[4].name}`:
+      messageCommands.hugMessageCommand(message, data.hugArray, invoker, userToInteract);
+    break;
 
-  // Command for patting a user
-  if ( (message.content.toLowerCase() === `${data.COMMAND_PREFIX}${data.commandArray[5].name}` && userToInteract) && message.member) {
-    await messageCommands.patMessageCommand(message, data.patArray, invoker, userToInteract);
-    return;
-  }
+    // Message command for patting the user (analogous to !pat)
+    case `${COMMAND_PREFIX}${data.commandArray[5].name}`:
+      messageCommands.patMessageCommand(message, data.patArray, invoker, userToInteract);
+    break;
 
-  // Command for kissing a user
-  if ( (message.content.toLowerCase() === `${data.COMMAND_PREFIX}${data.commandArray[6].name}` && userToInteract) && message.member) {
-    await messageCommands.kissMessageCommand(message, data.kissArray, invoker, userToInteract);
-    return;
-  }
+    // Message command for kissing the user (analogous to !kiss)
+    case `${COMMAND_PREFIX}${data.commandArray[6].name}`:
+      messageCommands.kissMessageCommand(message, data.kissArray, invoker, userToInteract);
+    break;
 
-  // Command for slapping a user
-  if ( (message.content.toLowerCase() === `${data.COMMAND_PREFIX}${data.commandArray[7].name}` && userToInteract) && message.member) {
-    await messageCommands.slapMessageCommand(message, data.slapArray, invoker, userToInteract);
-    return;
-  }
+    // Message command for slapping the user (analogous to !slap)
+    case `${COMMAND_PREFIX}${data.commandArray[7].name}`:
+      messageCommands.slapMessageCommand(message, data.slapArray, invoker, userToInteract);
+    break;
 
-  // Command for punching a user
-  if ( (message.content.toLowerCase() === `${data.COMMAND_PREFIX}${data.commandArray[8].name}` && userToInteract) && message.member) {
-    await messageCommands.punchMessageCommand(message, data.punchArray, invoker, userToInteract);
-    return;
-  } 
+    // Message command for punching the user (analogous to !punch)
+    case `${COMMAND_PREFIX}${data.commandArray[8].name}`:
+      messageCommands.punchMessageCommand(message, data.punchArray, invoker, userToInteract);
+    break;
 
-  // Command for bonking a user
-  if ( (message.content.toLowerCase() === `${data.COMMAND_PREFIX}${data.commandArray[9].name}` && userToInteract) && message.member) {
-    await messageCommands.bonkMessageCommand(message, data.bonkArray, invoker, userToInteract);
-  }
+    // Message command for bonking the user (analogous to !punch)
+    case `${COMMAND_PREFIX}${data.commandArray[9].name}`:
+      messageCommands.bonkMessageCommand(message, data.bonkArray, invoker, userToInteract);
+    break;
 
-  // Command for sending a random Ryan Gosling GIF
-  if ( (message.content.toLowerCase() === `${data.COMMAND_PREFIX}${data.commandArray[10].name}`) && message.member) {
-    await messageCommands.driveMessageCommand(message, data.driveArray);
-    return;
-  }
+    // Message command for displaying a random Ryan Gosling GIF.
+    case `${COMMAND_PREFIX}${data.commandArray[10].name}`:
+      messageCommands.driveMessageCommand(message, data.driveArray);
+    break;
 
-  // Command for nomming a user
-  if ( (message.content.toLowerCase() === `${data.COMMAND_PREFIX}${data.commandArray[11].name}` && userToInteract) && message.member) {
-    await messageCommands.nomMessageCommand(message, data.nomArray, invoker, userToInteract);
-    return;
-  }
+    // Message command for nomming the user :D
+    case`${COMMAND_PREFIX}${data.commandArray[11].name}`:
+      messageCommands.nomMessageCommand(message, data.nomArray, invoker, userToInteract);
+    break;
 
-  // Command for eating a user
-  if ( (message.content.toLowerCase() === `${data.COMMAND_PREFIX}${data.commandArray[12].name}` && userToInteract) && message.member) {
-    await messageCommands.nomMessageCommand(message, data.nomArray, invoker, userToInteract);
-    return;
-  }
-  
-  // Command for killing the user
-  if ( (message.content.toLowerCase() === `${data.COMMAND_PREFIX}${data.commandArray[13].name}` && userToInteract) && message.member) {
-    await messageCommands.killMessageCommand(message, data.killArray, data.slapArray, invoker, userToInteract);
-    return;
+    // Message command for eating the user (same as nom) :D
+    case`${COMMAND_PREFIX}${data.commandArray[12].name}`:
+      messageCommands.nomMessageCommand(message, data.nomArray, invoker, userToInteract);
+    break;
+
+    // Message command for killing the user D:
+    case`${COMMAND_PREFIX}${data.commandArray[13].name}`:
+      messageCommands.killMessageCommand(message, data.killArray, invoker, userToInteract);
+    break;
+
+    default: // No command case, I dont even think its needed to exist but oh well...
+    break;   // As a wise man once said: "If it works, DON'T CHANGE IT!!!" :P
+    
   }
 
 };
