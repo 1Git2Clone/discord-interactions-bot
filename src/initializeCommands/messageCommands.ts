832fc65143c783d21dea0faac508507055c4cdd2
1Kill2Steal <imliterallyhim@duck.com>
2024-01-21 15:40:47 +0200

    .

--- src/initializeCommands/messageCommands.ts
+++ src/initializeCommands/messageCommands.ts
@@ -139,6 +139,17 @@ async function kissMessageCommand(
 ): Promise<void> {
   if (!userToInteract) { message.reply('No user mentioned.'); return; }
 
+  if (invoker.id === "1119640244506087485") {
+    const embed = new EmbedBuilder()
+    .setColor('#ff6d66');
+    embed.setImage(kissArray[0]);
+  
+    await message.reply({
+      content: `*${invoker.toString()} kisses ${userToInteract.toString()}*`,
+      embeds: [embed],
+    });
+  }
+  
   if (userToInteract && invoker !== userToInteract) {
     const kissEmbed = getRandomEmbedElementFromArray(kissArray);
     await message.reply({
